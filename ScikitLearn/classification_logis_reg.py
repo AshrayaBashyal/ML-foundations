3f35760d14da8fc39e71ecb5363f0983ba59b8b6
AshrayaBashyal <ashrayabashyal1@gmail.com>
2025-09-23 18:39:14 +0545

    Classification with Logistic Regression basic idea

---
+++ ScikitLearn/classification_logis_reg.py
@@ -0,0 +1,14 @@
+# Classification with Logistic Regression
+# Regression → predicts continuous values (house price, exam score).
+# Classification → predicts discrete labels (spam / not spam, sick / healthy, pass / fail).
+
+# Logistic Regression Idea
+# It predicts probabilities using the sigmoid function:
+
+# 𝜎(𝑧)=1/(1+e)^(-z)
+
+# Output is between 0 and 1 (probability).
+# If probability > 0.5 → class 1
+# Else → class 0
+
+
