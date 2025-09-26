e44a2260f1dca8d3a6a0b184839fb9d135d83cf5
AshrayaBashyal <ashrayabashyal1@gmail.com>
2025-09-26 14:33:25 +0545

    Mini-Project Heart Disease Prediction

---
+++ ScikitLearn/Mini Project –Heart_Disease_Prediction.py
@@ -0,0 +1,15 @@
+# We’ll use the classic Heart Disease dataset (UCI Cleveland).
+# It has columns like:
+# age, sex, cp (chest pain type),
+# trestbps (resting blood pressure),
+# chol (cholesterol),
+# thalach (max heart rate),
+# target → 1 = disease, 0 = no disease.
+
+# Train all 3 models and compare metrics.
+# Try different hyperparameters:
+# max_depth for DecisionTree
+# n_neighbors for k-NN
+# penalty or C for Logistic Regression
+# Which model gives the best tradeoff between accuracy, precision, recall?
+# Bonus: Plot a bar chart of accuracy scores for all models.
